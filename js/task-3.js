7b769d55d22e783865d8e90915059b04c42cdc04
Kateryna D <july_info@ukr.net>
2024-05-14 00:23:22 +0300

    commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,10 @@
 function getElementWidth(content, padding, border) {
- 
-    return Number.parseFloat(content) + Number.parseFloat(border) * 2 + Number.parseFloat(padding) * 2;
+    widht = Number.parseFloat(content) + Number.parseFloat(border) * 2 + Number.parseFloat(padding) * 2;
+
+    return ` widht = ${widht}`;
 };
 
-
+console.log('Have a good day!');
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
